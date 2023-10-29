2b5f97c86af385d57d3549ec75dcade8641107c6
J-Krueger-Dev <Jkrueger212@gmail.com>
2023-10-29 16:22:54 -0600

    update and delete created

---
+++ src/components/rooms/delete/Delete.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Button} from "reactstrap";
+import { useState } from "react";
+
+function Delete({ token }) {
+  
+  const [roomName, setRoomname] = useState('');
+  const [description, setDescription] = useState('');
+
+
+  return (
+    <>
+      <Button color="dark" onClick={deleteRoom}>
+        Delete {/* TODO -- track owner_id */}
+      </Button>
+      
+    </>
+  );
+
+  async function deleteRoom (e) {
+    e.preventDefault();
+      console.log("room: ", roomName);
+      console.log("description: ", description);
+      ;
+
+      try {
+        let response = await fetch("http://localhost:4000/room/:id", {
+          headers: new Headers({
+            "content-type": "application/json",
+            "authorization": token
+          }),
+          method: "Delete",
+          body: JSON.stringify({
+            title: roomName,
+            description: description
+          }),
+        });
+
+        let results = await response.json();
+        console.log("results", results);
+  
+      } catch (error) {
+        console.log(error);
+      }
+  }
+}
+
+export default Delete;
