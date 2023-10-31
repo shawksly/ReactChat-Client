7a93b46d5bc8941450cf0030d3ff99571cd24b4d
shawksly <70265271+shawksly@users.noreply.github.com>
2023-10-30 20:03:27 -0600

    Update Delete.jsx

--- src/components/rooms/delete/Delete.jsx
+++ src/components/rooms/delete/Delete.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button} from "reactstrap";
 import { useState } from "react";
 
-function Delete({ token }) {
+function Delete({ token, currentRoomId, fetchRooms }) {
   
   const [roomName, setRoomname] = useState('');
   const [description, setDescription] = useState('');
@@ -21,20 +21,21 @@ function Delete({ token }) {
     e.preventDefault();
       console.log("room: ", roomName);
       console.log("description: ", description);
-      
+      ;
 
       try {
-        let response = await fetch("http://localhost:4000/room/:id", {
+        let response = await fetch(`http://localhost:4000/room/${currentRoomId}`, {
           headers: new Headers({
             "content-type": "application/json",
             "authorization": token
           }),
           method: "Delete"
-          
         });
 
         let results = await response.json();
         console.log("results", results);
+
+        fetchRooms();
   
       } catch (error) {
         console.log(error);
