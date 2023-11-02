f9ce50259a36fbaa6a7ea7c2c83f7bd4a962a71a
shawksly <shawksly@gmail.com>
2023-11-02 04:49:05 -0600

    U7_04 partial -- fixed icons, fixed messages displaying based on room change, and styled messages

---
+++ src/components/messages/updatemessage/UpdateMessage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UpdateMessage() {
+  return (
+    <i className="bi bi-feather m-1" role="button" style={{fontSize: "1rem", color: "black"}} onClick={null}></i>
+  )
+}
+
+export default UpdateMessage
