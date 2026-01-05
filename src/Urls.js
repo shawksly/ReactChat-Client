745d9e290f95ec1472fcd867c9a4b17de4682ec8
shawksly <shawksly@gmail.com>
2026-01-05 11:23:06 -0700

    Progress Backup

--- src/Urls.js
+++ src/Urls.js
@@ -1,2 +1,3 @@
-// export const baseUrl = `http://localhost:4000`
-export const baseUrl = `https://reactchat-server.onrender.com`
+export const baseUrl = `http://localhost:4000`
+// export const baseUrl = `https://reactchat-server.onrender.com`
+// export const baseUrl = `http://127.0.0.1:4003/api`
